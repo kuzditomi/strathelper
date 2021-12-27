37b24d3f658ae16e0d41278c88b3342347d5572b
Tamas Kuzdi <tamas.kuzdi@logmein.com>
2021-12-27 20:51:54 +0100

    list options

--- src/finder/Finder.component.tsx
+++ src/finder/Finder.component.tsx
@@ -1,33 +1,158 @@
-import { useState, useEffect } from 'react';
-import { getData } from './finder.api';
+import { useState, useEffect, useRef } from 'react';
+import { getData, TickerData } from './finder.api';
+import './finder.scss';
 
-function useFinder() {
+function useFinder(ticker: string) {
   const [isLoading, setIsLoading] = useState(false);
-  const [text, setText] = useState('');
+  const [data, setData] = useState<TickerData | undefined>();
 
   useEffect(() => {
+    if (!ticker) {
+      setData(undefined);
+      return;
+    }
     setIsLoading(true);
 
     (async () => {
       try {
-        const textResult = await getData('T');
+        const tickerData = await getData(ticker);
 
-        setText(Object.keys(textResult.callExpDateMap).join(','));
+        setData(tickerData);
       } finally {
         setIsLoading(false);
       }
     })();
-  }, []);
+  }, [ticker]);
 
-  return { isLoading, text };
+  return { isLoading, data };
+}
+
+interface Option {
+  type: 'C' | 'P';
+  strike: number;
+  expiry: string;
 }
 
 export const Finder: React.FC = () => {
-  const { isLoading, text } = useFinder();
+  const tickerRef = useRef<HTMLInputElement>(null);
+  const [ticker, setTicker] = useState('');
+  const [expiry, setExpiry] = useState('');
+  const [options, setOptions] = useState({} as Record<string, Option>);
+  const { isLoading, data } = useFinder(ticker);
 
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
+  const addOption = function (type: 'C' | 'P', strike: number, expiry: string) {
+    setOptions({
+      ...options,
+      [`${type}${strike}${expiry}`]: { type, strike, expiry },
+    });
+  };
 
-  return <p>{text}</p>;
+  const renderExpiries = () => {
+    if (isLoading) {
+      return <p>Loading...</p>;
+    }
+
+    if (!data) {
+      return null;
+    }
+
+    return (
+      <div className="expiries">
+        {data?.expiries.map((exp) => (
+          <button
+            key={exp}
+            onClick={() => {
+              setExpiry(exp);
+              setOptions({});
+            }}
+          >
+            {exp}
+          </button>
+        ))}
+      </div>
+    );
+  };
+
+  const OptionCell: React.FC<{
+    key: string;
+    strike: number;
+    expiry: string;
+    type: 'C' | 'P';
+  }> = ({ strike, type }) => {
+    return (
+      <td
+        align="center"
+        className={options[`${type}${strike}${expiry}`] ? 'plus' : ''}
+      >
+        <button
+          onClick={() => {
+            addOption(type, strike, expiry);
+          }}
+        >
+          {type}
+        </button>
+      </td>
+    );
+  };
+  const renderPrices = () => {
+    if (!expiry) {
+      return null;
+    }
+
+    return (
+      <div className="prices">
+        <table>
+          <tbody>
+            <tr>
+              {data?.prices[expiry].map((strike) => (
+                <OptionCell
+                  key={expiry + strike + 'C'}
+                  expiry={expiry}
+                  strike={strike}
+                  type="C"
+                />
+              ))}
+            </tr>
+            <tr>
+              {data?.prices[expiry].map((n) => (
+                <td align="center" key={n}>
+                  {n}
+                </td>
+              ))}
+            </tr>
+            <tr>
+              {data?.prices[expiry].map((strike) => (
+                <OptionCell
+                  key={expiry + strike + 'P'}
+                  expiry={expiry}
+                  strike={strike}
+                  type="P"
+                />
+              ))}
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <div className="ticker">
+        <label>
+          Ticker:
+          <input type="text" ref={tickerRef} />
+        </label>
+        <button
+          onClick={() => {
+            tickerRef?.current?.value && setTicker(tickerRef.current.value);
+          }}
+        >
+          GO
+        </button>
+      </div>
+      {renderExpiries()}
+      {renderPrices()}
+    </div>
+  );
 };
--- src/finder/finder.api.ts
+++ src/finder/finder.api.ts
@@ -1,17 +1,42 @@
+import { TMock } from './tmock';
 const baseURL =
   import.meta.env.VITE_API_ENV == 'local'
     ? 'http://localhost:3001'
     : 'https://kuzditomi-strathelper.herokuapp.com';
 
-interface OptionChain {
+interface OptionChainResult {
   callExpDateMap: Record<string, string>;
   putExpDateMap: Record<string, string>;
 }
 
-export async function getData(ticker: string) {
+export interface TickerData {
+  expiries: string[];
+  prices: Record<string, number[]>;
+}
+
+export async function getData(ticker: string): Promise<TickerData> {
   const price = `${baseURL}/price?ticker=${ticker}`;
   const res = await fetch(price);
-  const chain = (await res.json()) as OptionChain;
+  const chain = (await res.json()) as OptionChainResult;
 
-  return chain;
+  // const chain = TMock as unknown as OptionChainResult;
+
+  const expiries = Object.keys(chain.callExpDateMap).map(
+    (t) => t.split(':')[0]
+  );
+  const prices = Object.keys(chain.callExpDateMap).reduce(
+    (priceList, expiry) => {
+      priceList[expiry.split(':')[0]] = Object.keys(
+        chain.callExpDateMap[expiry]
+      ).map((s) => Number(s));
+
+      return priceList;
+    },
+    {} as Record<string, number[]>
+  );
+
+  return {
+    expiries,
+    prices,
+  };
 }
