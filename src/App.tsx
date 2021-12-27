d710e3d0c5064416ffc9508278d746bab60f96ef
Tamas Kuzdi <tamas.kuzdi@logmein.com>
2021-12-27 13:35:57 +0100

    he

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,4 @@
-import {
-  BrowserRouter,
-  Navigate,
-  Route,
-  Routes,
-  useLocation,
-  useNavigate,
-} from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { CalculatorPage } from './calculator/Calculator.component';
 import { Strategies } from './strategies/Strategies.component';
 import { Menu } from './menu/menu';
